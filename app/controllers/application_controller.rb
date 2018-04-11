ba4febf2aa78fc3a5ab4f267b92237a3f2e66353
04_Yamamoto_Shinji <shinji.yamamoto0828@gmail.com>
2018-04-11 03:15:46 +0000

    devise strong parameter error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected 
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:name, :profile, :buyer_flag, :seller_flag, :image)
+    end
+      
   protect_from_forgery with: :exception
   
     def current_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+  }
+  #devise_for :users
   resources 'users'
   resources 'requests'
   root 'pages#index'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  #get '/login', to: 'sessions#new'
+  #post '/login', to: 'sessions#create'
+  #delete '/logout', to: 'sessions#destroy'
+  
+  
 end
